a3b10dc39a81356008311ed503171f0b0476b9a1
Jessie Grosen <jmgrosen@gmail.com>
2024-11-10 23:51:55 -0500

    add more CPVs to the global list

--- saci_db/cpvs/__init__.py
+++ saci_db/cpvs/__init__.py
@@ -2,11 +2,19 @@ from .cpv01_mavlink_motors import MavlinkCPV
 from .cpv02_gps_move import GPSCPV
 from .cpv03_deauth_cpv import WiFiDeauthDosCPV
 # from .cpv04_icmp_cpv import IcmpFloodCPV
+# from .cpv05_track_cpv import ObjectTrackCPV
+from .cpv06_roll_over import RollOverCPV
+from .cpv07_compass_interference import CompassInterferenceCPV
+from .cpv08_web_stop import WebStopCPV
 
 
 CPVS = [
     MavlinkCPV(),
     GPSCPV(),
     WiFiDeauthDosCPV(),
-    # IcmpFloodCPV,
+    # IcmpFloodCPV(),
+    # ObjectTrackCPV(),
+    RollOverCPV(),
+    CompassInterferenceCPV(),
+    WebStopCPV(),
 ]
