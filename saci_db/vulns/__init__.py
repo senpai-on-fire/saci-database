50cbec114524f1e7ce61f7e89e7eabe03d05c534
Tiffany Bao <tiffany.yzh@gmail.com>
2024-11-12 15:27:10 -0500

    expose the ASP and WifiCreds Vuln

--- saci_db/vulns/__init__.py
+++ saci_db/vulns/__init__.py
@@ -1,3 +1,5 @@
 from .mavlink_mitm_vuln import MavlinkVuln01 as MavlinkCPSV
 from .mavlink_overflow_vuln import MavlinkOverflow
 from .sik_vuln import SiKAuthVuln01 as SiKCPSV
+from .knowncreds import WifiKnownCredsVuln
+from .noaps import NoAPSVuln
