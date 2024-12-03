0b02faa60ed18f6b18ff360507d9938b1fa1567f
HalimaBouzidi <fh_bouzidi@esi.dz>
2024-12-03 11:32:26 -0800

    Updated CPV07

--- saci_db/cpvs/cpv07_pmagnet_compass_dos.py
+++ saci_db/cpvs/cpv07_pmagnet_compass_dos.py
@@ -1,53 +1,62 @@
 from typing import List, Type
 
 from saci.modeling import CPV
-from saci.modeling.device import (ControllerHigh, CameraHigh,
-                                  MultiCopterMotorHigh, MultiCopterMotorAlgo, CyberComponentBase, TelemetryHigh,
-                                  Controller)
-from saci.modeling.device.compass import CompassSensorHigh, CompassSensor
-from saci.modeling.device.motor.steering import SteeringHigh, Steering
+from saci.modeling.device import (CyberComponentBase,Controller, Motor)
+from saci.modeling.device.compass import CompassSensor
+from saci.modeling.device.motor.steering import Steering
 from saci.modeling.state import GlobalState
-from saci_db.vulns.knowncreds import WifiKnownCredsVuln
 
-from saci_db.vulns.noaps import NoAPSVuln
+from saci_db.vulns.compass_spoofing import CompassSpoofingVuln
+from saci_db.vulns.weak_data_integrity_vuln import WeakDataIntegrityVuln
 
+from saci.modeling.attack.magnetic_attack_signal import MagneticAttackSignal
+from saci.modeling.attack.base_attack_impact import BaseAttackImpact
 
 class CompassInterferenceCPV(CPV):
+    
     NAME = "The magnet-on-the-compass-DoS CPV"
-    DESCRIPTION = """The rover uses the readings from a digital compass to determine when it has reached the desired 
-    heading. This heading is calculated from the current heading when the rover finishes its first 7m driving 
-    segment. Under normal operation the rover will turn the wheels left or right and continue to drive until the 
-    desired heading is reached, at which time it will straighten the wheels and begin its second 7m driving segment. 
-    A CPV can be exposed by affixing a small magnet to the top of the compass. This will cause the compass heading to 
-    be “fixed” and not reflect a change when the rover’s heading has changed. There is still some variability but 
-    appears to be in the 1%-5% range. The physical effect manifested by this CPV is that rover will drive in a circle 
-    while it is attempting to find it’s desired heading and never start the second 7m driving segment."""
-
+    
     def __init__(self):
-        # TODO: somehow indicate that we must have physical access?
         super().__init__(
             required_components=[
                 CompassSensor(),
                 Controller(),
                 Steering(),
+                Motor(),],
+            entry_component=CompassSensor(),
+            exit_component=Motor(),
+            
+            vulnerabilities=[CompassSpoofingVuln(), WeakDataIntegrityVuln()],
+
+            goals = [],
+
+            initial_conditions={
+                "Position": "Any",
+                "Heading": "Any",
+                "Speed": "Any", 
+                "Environment": "Any", 
+                "RemoteController": "On", 
+                "CPSController": "Moving",
+                "Operating mode": "Mission",},
+
+            attack_vectors = [MagneticAttackSignal(src='magnet', dst=CompassSpoofingVuln().component)], 
+            attack_requirements = "Powerful magnet with adequate shapes and dimensions",
+            attack_impacts = [BaseAttackImpact(category='Loss of control',
+                                               description='CPS drives in circles without stopping')],
+
+            exploit_steps=[
+            "Prepare a powerful magnet with adequate shapes and dimensions.",
+            "Install the magnet on top of the compass.",
+            "Until observing change in the compass readings, keep doing the following: Point the CPS device at a different direction then install the magnet on top of the compass.",
+            "Rotate the CPS device and observe that the compass readings do not significantly change as the CPS rotates.",
             ],
-            entry_component=CompassSensorHigh(),
-            vulnerabilities=[],
-            initial_conditions=[
-                "software state: on, rover idle"   ,
-            ],
-            final_conditions=[
-                "rover drives in circle",
-            ],
-            steps=[
-                "power rover on",
-                "install magnet on top of compass",
-                "give rover drive command",
-            ],
+
+            associated_files = [],
+            reference_urls = ["https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV006"],
         )
 
     def is_possible_path(self, path: List[CyberComponentBase]):
-        required_components = [CompassSensorHigh, ControllerHigh, SteeringHigh]
+        required_components = [CompassSensor, Controller, Steering, Motor]
         for required in required_components:
             if not any(map(lambda p: isinstance(p, required), path)):
                 return False
