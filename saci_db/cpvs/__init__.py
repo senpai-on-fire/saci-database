4b9d234f5f5818731f42a2b4bb9d52979b142edd
HalimaBouzidi <fh_bouzidi@esi.dz>
2024-12-02 21:18:53 -0800

    Controller and Telemetry

--- saci_db/cpvs/__init__.py
+++ saci_db/cpvs/__init__.py
@@ -1,11 +1,11 @@
 from .cpv01_mavlink_motors import MavlinkCPV
-from .cpv02_gps_move import GPSCPV
-from .cpv03_deauth_cpv import WiFiDeauthDosCPV
+from .cpv02_gps_position_move import GPSCPV
+from .cpv03_deauth_dos import WiFiDeauthDosCPV
 # from .cpv04_icmp_cpv import IcmpFloodCPV
-# from .cpv05_track_cpv import ObjectTrackCPV
-from .cpv06_roll_over import RollOverCPV
-from .cpv07_compass_interference import CompassInterferenceCPV
-from .cpv08_web_stop import WebStopCPV
+# from .cpv05_adv_ml_untrack import ObjectTrackCPV
+from .cpv06_serial_motor_rollover import RollOverCPV
+from .cpv07_pmagnet_compass_dos import CompassInterferenceCPV
+from .cpv08_wifi_webserver_stop import WebStopCPV
 
 
 CPVS = [
