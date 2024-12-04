39cd5269ca71870aaa196da47b47e6adb5663e52
Daniel-xsy <1491387884@qq.com>
2024-12-04 14:46:54 -0800

    Revise cpv05_adv_ml_attack

--- saci_db/cpvs/cpv05_adv_ml_untrack.py
+++ saci_db/cpvs/cpv05_adv_ml_untrack.py
@@ -1,12 +1,21 @@
 from typing import List, Type
 
 from saci.modeling import CPV
-from saci.modeling.device import (ControllerHigh, ObjectTracking, CameraHigh,
-                                  MultiCopterMotorHigh, MultiCopterMotorAlgo, CyberComponentBase)
+from saci.modeling.device import (
+    ControllerHigh,
+    DNN,
+    CameraHigh,
+    MultiCopterMotorHigh,
+    MultiCopterMotorAlgo,
+    CyberComponentBase,
+)
+from saci.modeling.communication import ExternalInput
 from saci.modeling.state import GlobalState
 
+from saci.modeling.attack.base_attack_vector import BaseAttackVector
+from saci.modeling.attack.image_attack_signal import ImageAttackSignal
+from saci.modeling.attack.base_attack_impact import BaseAttackImpact
 from saci_db.vulns.ml_vuln import DeepNeuralNetworkVuln
-
 
 class ObjectTrackCPV(CPV):
     NAME = "The Object Tracking CPV"
@@ -15,23 +24,66 @@ class ObjectTrackCPV(CPV):
         ml_vuln = DeepNeuralNetworkVuln()
         super().__init__(
             required_components=[
-                ml_vuln.component,
                 CameraHigh(),
                 ControllerHigh(),
                 MultiCopterMotorHigh(),
                 MultiCopterMotorAlgo(),
+                DNN(),
             ],
-            entry_component=CameraHigh(powered=True),
-            vulnerabilities=[ml_vuln]
+            entry_component=CameraHigh(),
+            exit_component=MultiCopterMotorHigh(),
+            vulnerabilities=[ml_vuln],
+            goals=[],
+            initial_conditions={
+                "Position": "Any",
+                "Heading": "Any",
+                "Speed": "Any",
+                "Environment": "Any",
+                "RemoteController": "Any",
+                "CPSController": "Any",
+                "Operating mode": "Autonomous",
+            },
+            attack_requirements=[
+                "Adversarial patches generated use adversarial machine learning",
+                "Decompiled DNN model from the firmware",
+            ],
+            attack_vectors=[
+                BaseAttackVector(
+                    name="Adversarial Pattern Injection",
+                    signal=ImageAttackSignal(
+                        src=ExternalInput(),
+                        dst=DeepNeuralNetworkVuln().component,
+                        modality="image",
+                    ),
+                    required_access_level="Remote",
+                    configuration={"duration": "transient"},
+                )
+            ],
+            attack_impacts=[
+                BaseAttackImpact(
+                    category="Control Manipulation",
+                    description=(
+                        "The attacker can manipulate the autonomous CPS behavior by injecting adversarial patterns "
+                    ),
+                ),
+            ],
+            exploit_steps=[
+                "Decompile the DNN model from the CPS firmware.",
+                "Dump the source code and model weight of the DNN model",
+                "Generate adversarial examples using adversarial machine learning-based optimization.",
+                "Showcase the adversarial examples to the CPS camera for control manipulation."
+            ],
+            associated_files=[],
+            reference_urls=[],
         )
+        # TODO: Attacker's goal state represented as the distorted object bounding box
+        self.goal_state_conditions = {"bounding_box": [0.0, 0.0]}
 
-        # TODO: how to model the shrinking effect of the object tracker
-        # we can compare with the initial one or simply check if the width and height are close to 0
-        self.goal_state_conditions = [0.0, 0.0]
+    def is_possible_path(self, path: List[Type[CyberComponentBase]]) -> bool:
+        for required in self.required_components:
+            if not any(map(lambda p: isinstance(p, required), path)):
+                return False
+        return True
 
-    def in_goal_state(self, state: GlobalState):
-        for component in state.components:
-            # the width and height are close to 0
-            # return of component.track(): [x, y, w, h]
-            if isinstance(component, ObjectTracking) and component.track()[-2:] == self.goal_state_conditions:
-                return True
+    def in_goal_state(self, state: GlobalState) -> bool:
+        pass
--- saci_db/vulns/ml_vuln.py
+++ saci_db/vulns/ml_vuln.py
@@ -7,8 +7,8 @@ Modeling the adversarial examples attack
 The modeled imapct is: attackers can control the output of DNN models
 '''''
 from saci.modeling import BaseVulnerability
-from saci.modeling.device import Device, ObjectTracking
-from saci.modeling.communication import AuthenticatedCommunication
+from saci.modeling.device import Device, DNN
+from saci.modeling.communication import UnauthenticatedCommunication
 
 class DeepNeuralNetworkPred(Predicate):
     pass
@@ -17,11 +17,11 @@ class DeepNeuralNetworkVuln(BaseVulnerability):
     def __init__(self):
         super().__init__(
             # Assuming that ObjectTracking is a DNN model
-            component=ObjectTracking(),
+            component=DNN(),
             # The input to a deauth attack is unauthenticated 
-            _input=AuthenticatedCommunication(),
+            _input=UnauthenticatedCommunication(),
             # The output is the disconnection 
-            output=AuthenticatedCommunication(),
+            output=UnauthenticatedCommunication(),
             attack_ASP=DeepNeuralNetworkPred,
             rulefile=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'deepneuralnetwork.lp')
         )
@@ -29,7 +29,7 @@ class DeepNeuralNetworkVuln(BaseVulnerability):
     def exists(self, device: Device) -> bool:
         for comp in device.components:
             # Check if the component have object tracking model
-            if isinstance(comp, ObjectTracking):
+            if isinstance(comp, DNN):
                 if comp.known_source and comp.known_weight:
                     # If we can dump the source code and the weight of the model, it's vulnerable
                     # The attacker can control the output of the model
