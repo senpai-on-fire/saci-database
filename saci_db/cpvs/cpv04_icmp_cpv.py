e7ba8bd7bfbcb16fd4cb9e006e2ea54c3c65dd2c
thebah <97621457+thebah@users.noreply.github.com>
2024-12-04 15:04:47 -0800

    icmp cpv

--- saci_db/cpvs/cpv04_icmp_cpv.py
+++ saci_db/cpvs/cpv04_icmp_cpv.py
@@ -1,35 +1,70 @@
+
 from typing import List, Type
 
-from saci.modeling.device.component import CyberComponentBase, Telemetry
-
 from saci.modeling import CPV
-from saci.modeling.device import Controller, Device
-from saci.modeling.state import GlobalState
-from saci_db.vulns.icmp_vuln import IcmpFloodVuln
+from saci.modeling.device import Controller, Wifi, Controller, Motor
+#from saci_db.vulns.icmp_vuln import IcmpFloodVuln
+from saci.modeling.communication import ExternalInput
+from saci.modeling.attack.packet_attack_signal import PacketAttackSignal
+from saci.modeling.attack.base_attack_vector import BaseAttackVector
+from saci.modeling.attack.base_attack_impact import BaseAttackImpact
 
-# Define the CPV for the ICMP Flood Attack
-class IcmpFloodCPV(CPV):
+from saci.modeling.state import GlobalState
+from saci_db.vulns.wifi_lack_auth_vuln import LackWifiAuthenticationVuln
+
+
+#This is to model the attack in the ARDrone drone as described by the referenced paper,
+
+class ICMPFloodingCPV(CPV):
+    
+    NAME = "ICMP flooding attack CPV"
+
     def __init__(self):
-        icmp_flood_vuln = IcmpFloodVuln()
         super().__init__(
             required_components=[
-                Telemetry(),
-                # NetworkComponent,
+                Wifi(),
                 Controller(),
+                Controller(),
+                Motor(),
             ],
-            entry_component=icmp_flood_vuln.component,
-            vulnerabilities=[icmp_flood_vuln]
-        )
+            entry_component = Wifi(),
+            exit_component = Motor(),
 
+            vulnerabilities =[LackWifiAuthenticationVuln()],
+
+            initial_conditions ={
+                "Position": "Any",
+                "Heading": "Any",
+                "Speed": "Any (>0)",
+                "Environment": "Any",
+                "RemoteController": "On",
+                "CPSController": "Moving",
+                "Operating mode": "Manual"
+            },
+            
+            attack_requirements=[
+                "Computer"
+                "WIFI card with monitor mode"
+                "Aircrack-ng software",
+            ],
+
+            attack_vectors = [BaseAttackVector(name="ICMP flooding attack", 
+                                               signal=PacketAttackSignal(src=ExternalInput(), dst=Wifi()),
+                                               required_access_level="Proximity",
+                                               configuration={"protocol":"UDP","port":"5556"},
+                                                )],
+            attack_impacts = [BaseAttackImpact(category='Denial of control',
+                                               description='The user can not control the CPS')],
+            exploit_steps=[
+                "Set the Wi-Fi card into monitor mode and find the BSSID and channel number for the CPS's Wi-Fi network.",
+                "join the network",
+                "Flood the CPS with TCP SYN on port UDP 5556"
+                ],
+                
+            associated_files=[],
+            reference_urls=["https://link.springer.com/article/10.1007/s11416-011-0158-4"]
+        )
+    
     def in_goal_state(self, state: GlobalState):
-        # The goal state is the network component being overwhelmed by ICMP traffic
-        for component in state.components:
-            if isinstance(component, self.entry_component) and component.protocol_name == "icmp":
-                # Check if the network component is overwhelmed by the ICMP flood
-                if component.powered and component.is_overwhelmed_by_icmp():
-                    # Check the state of the controller
-                    controller = next((comp for comp in state.components if isinstance(comp, Controller)), None)
-                    if controller:
-                        # Assuming there's an attribute 'emergency_state' that indicates the controller is in an emergency mode
-                        return controller.emergency_state
-        return False
+        # TODO?
+        pass
