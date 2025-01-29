a80ef22d64560a90aaf99cbbb9b16f0f79f923cc
HalimaBouzidi <fh_bouzidi@esi.dz>
2025-01-28 22:49:30 -0800

    Fixed Small Naming Issues

--- saci_db/cpvs/__init__.py
+++ saci_db/cpvs/__init__.py
@@ -8,7 +8,7 @@ from .cpv016_serial_motor_redirect import *
 from .cpv017_tmagnet_compass_disorient import *
 from .cpv018_smbus_battery_shutdown import *
 from .cpv019_debug_esc_flash import *
-from .cpv01_sik_mavlink_motors import *
+from .cpv001_sik_mavlink_motors import *
 from .cpv020_serial_esc_bootloader import *
 from .cpv021_serial_esc_reset import *
 from .cpv022_serial_esc_discharge import *
@@ -19,7 +19,7 @@ from .cpv026_acoustic_spoofing_accelerometer import *
 from .cpv027_acoustic_spoofing_gyroscope_dos import *
 from .cpv028_acoustic_spoofing_magnetometer import *
 from .cpv029_airflow_barometer_spoofing import *
-from .cpv02_gps_spoofing_move import *
+from .cpv002_gps_spoofing_move import *
 from .cpv030_projector_opticalflow_spoofing import *
 from .cpv031_laser_depthcamera_spoofing import *
 from .cpv032_emi_motor_block import *
@@ -30,7 +30,7 @@ from .cpv036_emi_motor_rotate import *
 from .cpv037_wifi_ardiscovery_flooding import *
 from .cpv038_wifi_ardiscovery_overflow import *
 from .cpv039_wifi_ardisovery_mitm import *
-from .cpv03_wifi_deauth_dos import *
+from .cpv003_wifi_deauth_dos import *
 from .cpv040_ftp_telnet_Hijack import *
 from .cpv041_wifi_beacon_flooding import *
 from .cpv042_rf_signal_jamming import *
@@ -41,7 +41,7 @@ from .cpv046_emi_comm_magnetometer import *
 from .cpv047_gps_directional_manipulation import *
 from .cpv048_gps_failsafe_avoidance import *
 from .cpv049_gps_path_manipulation import *
-from .cpv04_wifi_icmp_flooding import *
+from .cpv004_wifi_icmp_flooding import *
 from .cpv050_patch_emergency_stop_failure import *
 from .cpv051_patch_turn_malfunction import *
 from .cpv052_patch_unstable_attitude_control import *
@@ -52,12 +52,12 @@ from .cpv056_gnss_spoofing_loiter import *
 from .cpv057_dsmx_jamming_hijack import *
 from .cpv058_payload_command_crash import *
 from .cpv059_payload_disable_safety import *
-from .cpv05_adv_ml_untrack import *
+from .cpv005_adv_ml_untrack import *
 from .cpv060_payload_spoof_id import *
-from .cpv06_serial_motor_rollover import *
-from .cpv07_pmagnet_compass_dos import *
-from .cpv08_wifi_webserver_crash import *
-from .cpv09_gps_spoofing_static import *
+from .cpv006_serial_motor_rollover import *
+from .cpv007_pmagnet_compass_dos import *
+from .cpv008_wifi_webserver_crash import *
+from .cpv009_gps_spoofing_static import *
 
 
 CPVS = [
