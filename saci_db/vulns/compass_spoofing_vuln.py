98e578568ebdfb71547b1393f2b0dc3e8c84e46b
Daniel-xsy <1491387884@qq.com>
2025-03-03 15:06:09 -0800

    update compass_spoofing_vuln modeling

--- saci_db/vulns/compass_spoofing_vuln.py
+++ saci_db/vulns/compass_spoofing_vuln.py
@@ -36,14 +36,24 @@ class CompassSpoofingVuln(SpoofingVulnerability):
                 "CWE-693: Protection Mechanism Failure",
                 "CWE-1188: Insecure Default Initialization of Resource"
             ],
-            attack_vectors = [BaseAttackVector(name="Magnetic Signals Interference", 
-                                               signal=MagneticAttackSignal(src=ExternalInput(), dst=CompassSensor()),
-                                               required_access_level="Physical")],
-            exploits=['CompassTemporarySpoofingCPV', 'CompassPermanentSpoofingCPV'],
-            comp_attack_effect=BaseCompEffect(category='Integrity', description='Inject erroneous compass data'),
-            reference_urls=["https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV007",
-                            "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV006"]
-        )
+            attack_vectors = [
+                {
+                    "attack_vector": [BaseAttackVector(name="Magnetic Signals Interference", 
+                                                       signal=MagneticAttackSignal(src=ExternalInput(), dst=CompassSensor()),
+                                                       required_access_level="Physical")],
+                    "related_cpv": ['CompassTemporarySpoofingCPV', 'CompassPermanentSpoofingCPV'],
+                    "comp_attack_effect": BaseCompEffect(category='Integrity', description='Inject erroneous compass data'),
+                    "exploit_steps": [
+                        "Prepare a powerful magnet with adequate shapes and dimensions.",
+                        "Install the magnet on top of the compass.",
+                        "Until observing change in the compass readings, keep doing the following: Point the CPS device at a different direction then install the magnet on top of the compass.",
+                        "Rotate the CPS device and observe that the compass readings do not significantly change as the CPS rotates.",
+                    ],
+                    "reference_urls": ["https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV007",
+                                       "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV006"]
+                }
+            ]  
+        )   
 
     def exists(self, device: Device) -> bool:
         # Iterate through all components of the device
