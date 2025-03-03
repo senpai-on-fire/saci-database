9795c6808ed22e741f59821cb6121798db8b5d18
Daniel-xsy <1491387884@qq.com>
2025-03-03 14:37:55 -0800

    Update compass_spoofing_vuln

--- saci_db/vulns/compass_spoofing_vuln.py
+++ saci_db/vulns/compass_spoofing_vuln.py
@@ -2,10 +2,13 @@ import os.path
 
 from clorm import Predicate
 
+from saci.modeling.attack import BaseCompEffect
 from saci.modeling import BaseVulnerability
 from saci.modeling import SpoofingVulnerability
 from saci.modeling.device import Device
 from saci.modeling.device.sensor.compass import CompassSensor
+from saci.modeling.attack.base_attack_vector import BaseAttackVector
+from saci.modeling.attack.magnetic_attack_signal import MagneticAttackSignal
 from saci.modeling.communication import AuthenticatedCommunication, UnauthenticatedCommunication, ExternalInput
 
 # Predicate to define formal reasoning logic for compass spoofing attacks
@@ -32,9 +35,14 @@ class CompassSpoofingVuln(SpoofingVulnerability):
                 "CWE-20: Improper Input Validation",
                 "CWE-693: Protection Mechanism Failure",
                 "CWE-1188: Insecure Default Initialization of Resource"
-            ]
-
-          
+            ],
+            attack_vectors = [BaseAttackVector(name="Magnetic Signals Interference", 
+                                               signal=MagneticAttackSignal(src=ExternalInput(), dst=CompassSensor()),
+                                               required_access_level="Physical")],
+            exploits=['CompassTemporarySpoofingCPV', 'CompassPermanentSpoofingCPV'],
+            comp_attack_effect=BaseCompEffect(category='Integrity', description='Inject erroneous compass data'),
+            reference_urls=["https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV007",
+                            "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV006"]
         )
 
     def exists(self, device: Device) -> bool:
