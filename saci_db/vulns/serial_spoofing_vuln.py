f473d11ccb649d044bfaffed69d2e372432440df
Daniel-xsy <1491387884@qq.com>
2025-03-03 22:26:17 -0800

    [junchi] update component vulberability modeling

--- saci_db/vulns/serial_spoofing_vuln.py
+++ saci_db/vulns/serial_spoofing_vuln.py
@@ -2,9 +2,14 @@ import os.path
 from clorm import Predicate
 
 from saci.modeling import SpoofingVulnerability
+from saci.modeling.attack import BaseCompEffect
 from saci.modeling.device import Device, Serial
 from saci.modeling.communication import AuthenticatedCommunication, UnauthenticatedCommunication, ExternalInput
 from saci.modeling.communication.protocol import UARTProtocol, I2CProtocol, SPIProtocol
+from saci.modeling.attack.serial_attack_signal import SerialAttackSignal 
+from saci.modeling.attack.base_attack_vector import BaseAttackVector
+from saci.modeling.attack.base_attack_impact import BaseAttackImpact
+from saci.modeling.communication import ExternalInput
 
 # Predicate to define formal reasoning logic for serial spoofing attacks
 class SerialSpoofingPred(Predicate):
@@ -31,6 +36,72 @@ class SerialSpoofingVuln(SpoofingVulnerability):
                 "CWE-345: Insufficient Verification of Data Authenticity",
                 "CWE-20: Improper Input Validation",
                 "CWE-1188: Insecure Default Initialization of Resource"
+            ],
+            attack_vectors = [
+                {   # List of related attack vectors and their exploitation information:
+                    "attack_vector": [BaseAttackVector(name='Serial DSHOT and Arduino Command Injection',
+                                               signal=SerialAttackSignal(src=ExternalInput(), dst=Serial(), data='any'),
+                                               required_access_level='Physical',
+                                               configuration={"command_types": ["DSHOT", "Arduino", "Settings"]},
+                                               )],
+                    # List of associated CPVs
+                    "related_cpv": [
+                        "SerialRollOverCPV",
+                        "SerialRedirectCPV",
+                        "SerialThrottleCPV",
+                        "SerialArduinoControlCPV"
+                    ],
+                    # List of associated component-level attack effects
+                    "comp_attack_effect": [BaseCompEffect(category='Integrity',
+                                               description='Serial command injection can cause unauthorized device movement, excessive speed, and potential rollover')],
+                    # Steps of exploiting this attack vector
+                    "exploit_steps": [
+                        "Connect to device via serial interface",
+                        "Send specific commands (e.g., DSHOT commands) repeatedly in idle state",
+                        "Observe changes in device behavior"
+                    ],
+                    # List of related references
+                    "reference_urls": [
+                        "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV006",
+                        "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV0011",
+                        "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV014"
+                    ]
+                },
+                {
+                    "attack_vector": [BaseAttackVector(name="Serial ESC Command Injection",
+                                               signal=SerialAttackSignal(src=ExternalInput(), dst=Serial()),
+                                               required_access_level="Physical",
+                                               )],
+                    # List of associated CPVs
+                    "related_cpv": [
+                        "SerialESCBootloaderCPV",
+                        "SerialESCResetCPV",
+                        "SerialESCDischargeCPV",
+                        "SerialESCOverflowCPV",
+                        "SerialESCExeccmdCPV",
+                        "SerialOverheatingCPV"
+                    ],
+                    # List of associated component-level attack effects
+                    "comp_attack_effect": [BaseCompEffect(category='Integrity',
+                                               description='Serial ESC command injection can cause motor failure, battery discharge issues, and potential hardware damage through overheating or buffer overflow')],
+                    # Steps of exploiting this attack vector
+                    "exploit_steps": [
+                        "Connect to ESC via USB-TTL serial adapter",
+                        "Send configuration modification commands",
+                        "Observe abnormal device behavior"
+                    ],
+                    # List of related references
+                    "reference_urls": [
+                            "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV008",
+                            "https://github.com/neoxic/ESCape32/wiki/Configuration#cli-command-line-interface",
+                            "https://github.com/senpai-on-fire/Greensight1A-esc-CPVs/blob/main/CPV003/HII-GSP1AESC01NR017-CPV003-20241002.docx",
+                            "https://github.com/senpai-on-fire/Greensight1A-esc-CPVs/blob/main/CPV004/HII-GSP1AESC01NR017-CPV004-20241003.docx",
+                            "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV005",
+                            "https://github.com/neoxic/ESCape32/wiki/Configuration#cli-command-line-interface",
+                            "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV006",
+                            "https://github.com/senpai-on-fire/NGC1B-rover-CPVs/tree/main/CPV007"
+                    ]
+                }
             ]
         )
 
